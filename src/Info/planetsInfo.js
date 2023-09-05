375255d86c1d31ea0a8afefce2842f7fef584af9
Christian Rivero <christianriveroarnez@gmail.com>
2023-09-05 17:31:24 -0400

    added rover file

--- src/Info/planetsInfo.js
+++ src/Info/planetsInfo.js
@@ -51,14 +51,46 @@ const planets = {
     
   },
   voyager1: {
-    name: 'Voyager1',
+    name: 'Voyager 1',
     description:
-      'Neptune is the eighth and farthest known planet from the Sun in the Solar System. It is a gas giant and is often referred to as the "Blue Giant" because of its blue appearance.',
+      'Voyager 1 is a space probe launched by NASA in 1977. It is the farthest human-made object from Earth and has traveled beyond our solar system.',
     texture: '/textures/Voyager.glb',
-    type:"ship",
-
-    
+    type: 'ship',
   },
+  apollo11: {
+    name: 'Apollo 11',
+    description:
+      'Apollo 11 was the first manned mission to land on the Moon in 1969. Neil Armstrong and Buzz Aldrin became the first humans to walk on the lunar surface.',
+    model: '/models/apollo-11.glb',
+    type: 'ship',
+  },
+  hubbleTelescope: {
+    name: 'Hubble Space Telescope',
+    description:
+      'The Hubble Space Telescope is a powerful space observatory launched by NASA in 1990. It has provided stunning images and valuable data about our universe.',
+    model: '/models/hubble-telescope.glb',
+    type: 'ship',
+  },
+  marsRover: {
+    name: 'Mars Rover',
+    description:
+      'Mars Rovers are robotic vehicles designed to explore the surface of Mars. They have provided valuable information about the Martian landscape and potential for life.',
+    model: '/models/mars-rover.glb',
+    type: 'ship',
+  },
+  internationalSpaceStation: {
+    name: 'International Space Station',
+    description:
+      'The International Space Station (ISS) is a space station and research laboratory in low Earth orbit. It s a joint project involving multiple space agencies.',
+    model: '/models/international-space-station.glb',
+    type: 'ship',
+  },
+  
+  
+  
+  
+  
+  
 };
 
 export default planets;
