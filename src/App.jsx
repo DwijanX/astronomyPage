0dd3b3cc3e4c740438c3fa84e28a3278d2be4daf
Christian Rivero <christianriveroarnez@gmail.com>
2023-09-05 18:37:11 -0400

    modified default route

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     },
     {
       path: '/',
-      element: <Navigate to="/planet/earth" replace />,
+      element: <Navigate to="/celestialBody/earth" replace />,
     },
     
   ]);
